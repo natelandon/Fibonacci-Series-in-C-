f31163169987dc93f17c152d76be04cddbc78bff
Nate Landon <druidprogramming@gmail.com>
2018-03-22 11:30:01 -0600

    Add files via upload

---
+++ fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max, add = 1, i = 0, j = 1;
+            Console.Write("Please Enter the Maximum Limit : ");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i);
+
+            while (add <= max)
+            {
+                Console.WriteLine(add);
+                add = i + j;
+                i = j;
+                j = add;
+            }
+        }
+    }
+}
